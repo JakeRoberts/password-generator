57f525d16d11612e500d131032e09463bf28f571
Rob <r.jacobs790@gmail.com>
2021-03-26 22:28:28 -0400

    Created a parsInt and commented what each line of code does

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,27 @@
 // Assignment Code
-var specialCharacter = ['@', '!', '#', '$', '%', '^', '&', '*', '<', '?', '+', '~', '_', '-', '/', '|', '{', '}', '[', ']']
+var specChar = ['@', '!', '#', '$', '%', '^', '&', '*', '<', '?', '+', '~', '_', '-', '/', '|', '{', '}', '[', ']']
 
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var capitalLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-var numericalValue = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var numerials = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-// push and concat will help for array
-// function numericalVal () {
-//  console.log(String.numericalValue(Math.floor(Math.random() * 10) + 3));
-// } may need for later
 
-// array to push things to. once character set determined set for loop. combine loop with math.random function
-var characterSet = [];
-
-// concat this
+// array to push things to.
+var newSet = [];
 
 
 
+
+// Going into HTML to select ID.
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Creating a function for developing your password.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+// Stating the value of passwordText.
   passwordText.value = password;
 
 
@@ -33,45 +29,55 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+// Created a function to generate password
 function generatePassword() {
 
+  // Creates a window to display message
   var passwordLength = prompt("Put the length of your password");
+  // Conditional stating if the password is less than 8 or more than 128 you will get an alert telling you it's not correct.
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Not sufficent")
     return;
   }
+  
+  //Conditional stating if the previous condition is false and not a number, will then create a new statement to alert the user of their mistake.
+  else if (isNaN(parseInt(passwordLength))) {
+      alert("Enter number between 8 and 128");
+      return;
+  }
+
+  // If the condition above is false, will create a new code.
   else {
     var hasNumbers = confirm("Include Numbers?");
-    var hasSpecialCharacters = confirm("Includes Special Characters");
-    var hasUpperCase = confirm("Includes Uppercase");
-    var hasLowerCase = confirm("Include Lowercase");
-
+    var hasspecChars = confirm("Includes Special Characters");
+    var hascapitalLetters = confirm("Includes capitalLetters");
+    var haslowerLetters = confirm("Include lowerLetters");
+ 
+    // Confirm statement
     if (hasNumbers) {
-      characterSet = characterSet.concat(numericalValue)
+      newSet = newSet.concat(numerials)
     }
-    if (hasSpecialCharacters) {
-      characterSet = characterSet.concat(specialCharacter)
+    if (hasspecChars) {
+      newSet = newSet.concat(specChar)
     }
-    if (hasUpperCase) {
-      characterSet = characterSet.concat(upperCase)
+    if (hascapitalLetters) {
+      newSet = newSet.concat(capitalLetters)
     }
-    if (hasLowerCase) {
-      characterSet = characterSet.concat(lowerCase)
+    if (haslowerLetters) {
+      newSet = newSet.concat(lowerLetters)
     }
   }
 
+  // Creating a new string :)
   var newString = "";
 
+  //Constantly looping to encompass all global variables to generate a random character
   for (i = 0; i < passwordLength; i++) {
-    var index = Math.floor(Math.random() * characterSet.length);
-    newString += characterSet[index];
+    var index = Math.floor(Math.random() * newSet.length);
+    newString += newSet[index];
   }
 
-
+  //Telling the String to Stop.
   return newString;
 }
-
-
-
 
